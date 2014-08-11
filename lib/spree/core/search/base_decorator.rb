ef90670349a39e0042a7b03b71a9f5b9cd4a8185
Jonathan Tapia <jonathan.tapia@crowdint.com>
2014-08-11 15:59:47 -0500

    Decorate base search method to use as default without_inactive scope

--- lib/spree/core/search/base_decorator.rb
+++ lib/spree/core/search/base_decorator.rb
@@ -1,14 +1,8 @@
 Spree::Core::Search::Base.class_eval do
 
-  # Redefine method to add properly scope to don't show inactive products
+  # Redefine method to add a properly scope to don't show inactive products
   def retrieve_products
-    @products_scope = get_base_scope
-    curr_page = page || 1
-
-    @products = @products_scope.includes([:master => :prices])
-    unless Spree::Config.show_products_without_price
-      @products = @products.where("spree_prices.amount IS NOT NULL").where("spree_prices.currency" => current_currency)
-    end
+    super
     @products = @products.without_inactive.page(curr_page).per(per_page)
   end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -27,6 +27,8 @@ Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
 require 'spree/testing_support/factories'
 require 'spree/testing_support/controller_requests'
 require 'spree/testing_support/authorization_helpers'
+require 'spree/testing_support/capybara_ext'
+require 'spree/testing_support/preferences'
 require 'spree/testing_support/url_helpers'
 
 # Requires factories defined in lib/spree_product_activator/factories.rb
