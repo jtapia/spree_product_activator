eeee25a736d62751747d8fa0dba65b91fb5b4aa2
Jonathan Tapia <jonathan.tapia@crowdint.com>
2014-08-12 11:34:20 -0500

    Remove unnecessary code

--- lib/spree/core/search/base_decorator.rb
+++
@@ -1,9 +0,0 @@
-Spree::Core::Search::Base.class_eval do
-
-  # Redefine method to add a properly scope to don't show inactive products
-  def retrieve_products
-    super
-    @products = @products.without_inactive.page(curr_page).per(per_page)
-  end
-
-end
